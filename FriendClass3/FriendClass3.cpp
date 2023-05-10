c3bbc4e0aaa9f8e2f7c06aa7e0e8321ea4646b29
Amandaptr44 <amandaptr86@gmail.com>
2023-05-10 16:21:44 +0700

    membuat method class pelajar

--- FriendClass3/FriendClass3.cpp
+++ FriendClass3/FriendClass3.cpp
@@ -9,3 +9,12 @@ public:
 	void showNilaipelajar(pelajar&);
 };
 
+class pelajar {
+private:
+	int nilai;
+public:
+	pelajar() {
+		nilai = 100;
+	}
+	friend void manusia::showNilaipelajar(pelajar& x);
+};
